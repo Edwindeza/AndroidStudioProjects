fdbaed1961a1dde3ccb7fb9341db6eb4f5e612fa
edwindeza <ejdeza@hotmail.com>
2016-12-21 22:17:57 -0500

    proyectos casa

--- MisContactos/app/src/main/java/com/example/dise07/miscontactos/DetalleContacto.java
+++ MisContactos/app/src/main/java/com/example/dise07/miscontactos/DetalleContacto.java
@@ -1,9 +1,20 @@
 package com.example.dise07.miscontactos;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class DetalleContacto extends AppCompatActivity {
+
+    private TextView tvNombre;
+    private TextView tvTelefono;
+    private TextView tvEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +26,33 @@ public class DetalleContacto extends AppCompatActivity {
         String telefono = parametros.getString(getResources().getString(R.string.ptelefono));
         String email = parametros.getString(getResources().getString(R.string.pemail));
 
+        tvNombre = (TextView) findViewById(R.id.tvNombre);
+        tvTelefono = (TextView) findViewById(R.id.tvTel);
+        tvEmail = (TextView) findViewById(R.id.tvEmail);
+
+        tvNombre.setText(nombre);
+        tvTelefono.setText(telefono);
+        tvEmail.setText(email);
 
     }
+
+    public void llamar(View v) {
+        String telefono = tvTelefono.getText().toString();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        startActivity(new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + telefono))); // El teléfono es un recurso tipo URI
+    }
+
+    public void enviarMail(View v){
+
+    }
+
 }
