39a70a51cf30177a4e559c740dc57c22567ffab8
edwindeza <ejdeza@hotmail.com>
2016-12-26 11:27:36 -0500

    proyectos

--- MisContactos/app/src/main/java/com/example/dise07/miscontactos/Contacto.java
+++ MisContactos/app/src/main/java/com/example/dise07/miscontactos/Contacto.java
@@ -9,11 +9,23 @@ public class Contacto {
     private String nombre;
     private String tlefono;
     private String email;
+    private int foto;
 
-    public Contacto(String nombre, String tlefono, String email) {
-        this.nombre = nombre;
-        this.tlefono = tlefono;
-        this.email = email;
+
+    public Contacto(String nombre, String tlefono, String email, int foto) {
+        this.nombre     = nombre;
+        this.tlefono    = tlefono;
+        this.email      = email;
+        this.foto       = foto;
+
+    }
+
+    public int getFoto() {
+        return foto;
+    }
+
+    public void setFoto(int foto) {
+        this.foto = foto;
     }
 
     public String getTlefono() {
