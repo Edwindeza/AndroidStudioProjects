e0ae44bd560529c1b531720a6f871b48331ae648
edwindeza <ejdeza@hotmail.com>
2016-12-26 14:47:35 -0500

    proyectos

---
+++ MisContactos/app/src/main/java/com/example/dise07/miscontactos/ContactoAdaptador.java
@@ -0,0 +1,71 @@
+package com.example.dise07.miscontactos;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+/**
+ * Created by Dise07 on 26/12/2016.
+ */
+
+public class ContactoAdaptador extends RecyclerView.Adapter<ContactoAdaptador.ContactoViewHolder>{
+    ArrayList<Contacto> contactos;
+
+    public ContactoAdaptador(ArrayList<Contacto> contactos) {
+        this.contactos = contactos;
+    }
+
+
+
+
+    //Inflar ellayout y lo pasará al viewholder para que le obtenga la lista
+    @Override
+    public ContactoViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.cardview_contacto, parent, false);
+        //Esta linea de codigo asocia el layour cardview_contacto con el MainActivity
+
+        return new ContactoViewHolder(v);
+    }
+
+
+
+
+    //asocia cada elementode la lista con cada view
+
+    @Override
+    public void onBindViewHolder(ContactoViewHolder contactoViewHolder, int position) {
+        //Aca pasamos la lista de contactos
+        Contacto contacto = contactos.get(position);
+
+        contactoViewHolder.imgFoto.setImageResource(contacto.getFoto());
+        contactoViewHolder.tvNombreCV.setText(contacto.getNombre());
+        contactoViewHolder.tvTelefonoCV.setText(contacto.getTlefono());
+
+    }
+
+    @Override
+    public int getItemCount() { //Cantidad de elementos que contiene la cardviewe
+        return contactos.size();
+    }
+
+    public static class ContactoViewHolder extends RecyclerView.ViewHolder{
+        //Esta clase es la que interacciona con el layout
+        private ImageView imgFoto;
+        private TextView tvNombreCV;
+        private TextView tvTelefonoCV;
+
+        public ContactoViewHolder(View itemView) {
+            super(itemView);
+            imgFoto         =(ImageView) itemView.findViewById(R.id.imgfotocv);
+            tvNombreCV      = (TextView) itemView.findViewById(R.id.tvNombreCV);
+            tvTelefonoCV    = (TextView) itemView.findViewById(R.id.tvTelefonoCV);
+        }
+    }
+
+}
--- MisContactos/app/src/main/java/com/example/dise07/miscontactos/MainActivity.java
+++ MisContactos/app/src/main/java/com/example/dise07/miscontactos/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.dise07.miscontactos;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -13,25 +15,44 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     ArrayList<Contacto>  contactos;
+    private RecyclerView listacontactos;
+
+    @Override
+    public void closeOptionsMenu() {
+        super.closeOptionsMenu();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        listacontactos = (RecyclerView) findViewById(R.id.rvContactos);
+
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+
+        listacontactos.setLayoutManager(llm);
+        inicializarListaContactos();
+        inicializarLizeAdaptador();
+
+
+
+        /*
         contactos = new ArrayList<Contacto>();
-        contactos.add(new Contacto("Edwin Deza", "987645213", "ejdeza@gmail.com"));
-        contactos.add(new Contacto("Darwin Deza", "234453366", "dezacul@gmail.com"));
-        contactos.add(new Contacto("Katherinne Deza", "684963215", "katdc_70@gmail.com"));
-        contactos.add(new Contacto("Junior Deza", "975409685", "ejdeza@hotmil.com"));
-        contactos.add(new Contacto("Juvenal Deza", "990117206", "serdeza@gmail.com"));
+        contactos.add(new Contacto("Edwin Deza", "987645213", "ejdeza@gmail.com",R.drawable.perfil));
+        contactos.add(new Contacto("Darwin Deza", "234453366", "dezacul@gmail.com",R.drawable.perfil));
+        contactos.add(new Contacto("Katherinne  Deza", "684963215", "katdc_70@gmail.com",R.drawable.perfil));
+        contactos.add(new Contacto("Junior Deza", "975409685", "ejdeza@hotmil.com",R.drawable.perfil));
+        contactos.add(new Contacto("Juvenal Deza", "990117206", "serdeza@gmail.com",R.drawable.perfil));
 
         ArrayList<String> nombresContacto= new ArrayList<>();
+
+
 
         for (Contacto contacto: contactos) {
             nombresContacto.add(contacto.getNombre());
         }
-
         ListView lstContactos = (ListView) findViewById(R.id.lstContactos);
         lstContactos.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, nombresContacto));
 
@@ -47,5 +68,21 @@ public class MainActivity extends AppCompatActivity {
                 //Intent explicito que permite unir componentes de una pantalla a otra.
             }
         });
+        */
+    }
+
+    public void inicializarLizeAdaptador(){
+        ContactoAdaptador adaptador=new ContactoAdaptador(contactos);
+        listacontactos.setAdapter(adaptador);
+    }
+
+    public void inicializarListaContactos(){
+        contactos = new ArrayList<Contacto>();
+        contactos.add(new Contacto("Edwin Deza", "987645213", "ejdeza@gmail.com",R.drawable.perfil));
+        contactos.add(new Contacto("Darwin Deza", "234453366", "dezacul@gmail.com",R.drawable.perfil));
+        contactos.add(new Contacto("Katherinne  Deza", "684963215", "katdc_70@gmail.com",R.drawable.perfil));
+        contactos.add(new Contacto("Junior Deza", "975409685", "ejdeza@hotmil.com",R.drawable.perfil));
+        contactos.add(new Contacto("Juvenal Deza", "990117206", "serdeza@gmail.com",R.drawable.perfil));
+
     }
 }
